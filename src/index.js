691c410445b8f7470cca27e3709da3e573840b70
Nguyễn Lê Hoài Ân <38518953+hoaian0505@users.noreply.github.com>
2019-12-16 14:45:58 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios';
 import Select from 'react-select';
 import { BallBeat } from 'react-pure-loaders';
